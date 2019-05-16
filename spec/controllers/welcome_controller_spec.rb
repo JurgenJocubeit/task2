b613aa21100bee26aa613545cb9c3fb30940d6cf
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2019-05-16 14:08:20 +1000

    Refactored specs. All passing.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,17 +3,15 @@ require 'uri'
 
 RSpec.describe WelcomeController, type: :controller do
   context 'when welcome params are blank' do
-    it 'sets @cronuts to the default value' do
-      get :index
+    before { get :index }
 
+    it 'sets @cronuts to the default value' do
       expect(assigns(:cronuts_url)).to eq(URI.escape(
         'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%' \
         'AE_2_Burnt_Vanilla_Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'))
     end
 
     it 'sets @donuts to the default value' do
-      get :index
-
       expect(assigns(:donuts_url)).to eq(URI.escape(
         'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg' \
         '/250px-Glazed-Donut.jpg'))
@@ -21,20 +19,26 @@ RSpec.describe WelcomeController, type: :controller do
   end
 
   context 'when welcome params are not blank' do
+    before do
+      get :index, {
+        params: {
+          cronuts_url: 'http://www.google.com',
+          donuts_url: 'http://www.google.com',
+          unpermitted_param: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        }
+      }
+    end
+
     it "only permits whitelisted params" do
-      expect(assigns(:unknown_attribute)).to be_nil
+      expect(assigns(:unpermitted_param)).to be_nil
     end
 
     it 'sets @cronuts to the cronuts_url param value' do
-      get :index, params: { cronuts_url: 'http://www.google.com' }
-
-      expect(assigns(:cronuts_url)).to eq 'http://www.google.com'
+      expect(assigns(:cronuts_url)).to eq(URI.escape('http://www.google.com'))
     end
 
     it 'sets @donuts to the donuts_url param value' do
-      get :index, params: { donuts_url: 'http://www.google.com' }
-
-      expect(assigns(:donuts_url)).to eq 'http://www.google.com'
+      expect(assigns(:donuts_url)).to eq(URI.escape('http://www.google.com'))
     end
   end
 end
