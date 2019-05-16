9fecebf25446d8ad027b541c62c0d28fdc80a984
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2019-05-16 11:38:53 +1000

    Add permitted params check and unit test.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   	# Use the cronuts_url parameter as the cronuts link if the param is available
-  	@cronuts_url = !params[:cronuts_url].nil? ? params[:cronuts_url] : "https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg"
+  	@cronuts_url = !welcome_params[:cronuts_url].nil? ? welcome_params[:cronuts_url] : "https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg"
   	# Use the donuts_url parameter as the donuts link if the param is available
-  	@donuts_url = !params[:donuts_url].nil? ? params[:donuts_url] : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut.jpg"
+  	@donuts_url = !welcome_params[:donuts_url].nil? ? welcome_params[:donuts_url] : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut.jpg"
+  end
+
+  private
+
+  def welcome_params
+    params.permit(:cronuts_url, :donuts_url)
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  context 'when params cronuts_url is blank' do
+  context 'when welcome params are blank' do
     it 'sets @cronuts to the default value' do
       get :index
 
       expect(assigns(:cronuts_url)).to eq 'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'
     end
-  end
 
-  context 'when params cronuts_url is not blank' do
-    it 'sets @cronuts to the cronuts_url param value' do
-      get :index, params: { cronuts_url: 'http://www.google.com' }
-
-      expect(assigns(:cronuts_url)).to eq 'http://www.google.com'
-    end
-  end
-
-  context 'when params donuts_url is blank' do
     it 'sets @donuts to the default value' do
       get :index
 
@@ -25,7 +15,17 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
-  context 'when params donuts_url is not blank' do
+  context 'when welcome params are not blank' do
+    it "only permits whitelisted params" do
+      expect(assigns(:unknown_attribute)).to be_nil
+    end
+
+    it 'sets @cronuts to the cronuts_url param value' do
+      get :index, params: { cronuts_url: 'http://www.google.com' }
+
+      expect(assigns(:cronuts_url)).to eq 'http://www.google.com'
+    end
+
     it 'sets @donuts to the donuts_url param value' do
       get :index, params: { donuts_url: 'http://www.google.com' }
 
